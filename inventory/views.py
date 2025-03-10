08fd7f441ed53d67c9bdccba3dce564c555ea23b
Edmond Kodua <aatidua@gmail.com>
2025-03-10 19:00:06 -0300

    Doing well so far

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Ingredients, MenuItems, RecipeRequirement
+from django.views.generic import ListView, DetailView
+
+class IngredientsList(ListView):
+    model = Ingredients
+    template_name = 'inventory/ingredients_list.html'
+
+class MenuItemsList(ListView):
+    model = MenuItems
+    template_name = 'inventory/menu_items_list.html'
+
+class RecipeRequirementList(ListView):
+    model = RecipeRequirement
+    template_name = 'inventory/recipe_requirement_list.html'
+
+print(dir(ListView))
